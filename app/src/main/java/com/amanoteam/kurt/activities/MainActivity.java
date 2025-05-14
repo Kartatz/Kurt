d9228b50361b1562f3401e62fec2ebad91e69dea
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-05-13 22:38:00 -0300

    .

--- app/src/main/java/com/amanoteam/kurt/activities/MainActivity.java
+++ app/src/main/java/com/amanoteam/kurt/activities/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 	public void onBackPressed() {
 		final WebView webView = viewModel.getWebView();
 		
-		Fragment f = getActivity().getFragmentManager().findFragmentById(R.id.fragment_container_view);
+		Fragment f = getFragmentManager().findFragmentById(R.id.fragment_container_view);
 		
 		if (f instanceof HomeFragment && webView.canGoBack()) {
 			webView.goBack();
