9a39e2321e260328523bd78d7b72456bf7934ea6
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-05-22 19:03:49 -0300

    .

--- app/src/main/java/com/amanoteam/kurt/fragments/HomeFragment.java
+++ app/src/main/java/com/amanoteam/kurt/fragments/HomeFragment.java
@@ -340,7 +340,7 @@ public class HomeFragment extends Fragment {
 		webView = (WebView) fragmentView.findViewById(R.id.webview);
 		final WebSettings webSettings = webView.getSettings();
 		
-		snackbar = Snackbar.make(activity, fragmentView, null, Snackbar.LENGTH_SHORT);
+		snackbar = Snackbar.make(activity.findViewById(android.R.id.content), null, Snackbar.LENGTH_SHORT);
 		final View snackbarView = (View) snackbar.getView();
 
 		final LayoutParams params = (LayoutParams) snackbarView.getLayoutParams();
